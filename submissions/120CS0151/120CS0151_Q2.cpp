82e839f129d93add079bae3d16ff7f01b363be09
deepti-02 <88343458+deepti-02@users.noreply.github.com>
2021-08-03 20:46:51 +0530

    Create 120CS0151_Q2.cpp

---
+++ submissions/120CS0151/120CS0151_Q2.cpp
@@ -0,0 +1,38 @@
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  {
+        //code here
+        set<int> res;
+        
+        for(int i=0; i<n; i++){
+            res.insert(a[i]);
+        }
+        
+        for(int i=0; i<m; i++){
+            res.insert(b[i]);
+        }
+        
+        return res.size();
+    }
+};
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, m;
+	    cin >> n >> m;
+	    int a[n], b[m];
+	   
+	    for(int i = 0;i<n;i++)
+	       cin >> a[i];
+	       
+	    for(int i = 0;i<m;i++)
+	       cin >> b[i];
+	    Solution ob;
+	    cout << ob.doUnion(a, n, b, m) << endl;
+	    
+	}
